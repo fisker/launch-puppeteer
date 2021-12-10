98dfc57ebcc6fa6cd9d3b25f3fff710a35e55c55
fisker Cheung <lionkay@gmail.com>
2021-12-10 19:30:00 +0800

    Switch to `uvu`

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-import test from 'ava'
+import {test} from 'uvu'
+import * as assert from 'uvu/assert'
 import launchBrowser from './index.js'
 
-test('main', async (t) => {
+test('main', async () => {
   const browser = await launchBrowser()
 
   const page = await browser.newPage()
   await page.goto('about:blank')
   const width = await page.evaluate(() => window.innerWidth)
 
-  t.is(width, 800)
+  assert.is(width, 800)
 })
